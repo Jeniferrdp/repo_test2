5672d1a3daf233fe946d72f32c79db2fa6cb2ae4
angelo <nodeands292@gmail.com>
2026-05-19 14:53:43 -0400

    oi

---
+++ tests/test_google.py
@@ -0,0 +1,15 @@
+from playwright.sync_api import sync_playwright
+
+def test_google():
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        
+        page = browser.new_page()
+
+        page.goto("https://www.google.com")
+
+        print("\nTitle:", page.title())
+
+        assert "Google" in page.title()
+
+        browser.close()
